e5b72028da1b48910a1cb9b0d6d57033dd39ed21
Sugendran Ganess <sugendran@sugendran.net>
2017-04-22 14:31:48 +0100

    changed deploy to push to S3

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,1 @@
-scp index.html humans.txt robots.txt root@www.sugendran.com:/var/local/webserver
-scp -r styles root@www.sugendran.com:/var/local/webserver
-scp -r scripts root@www.sugendran.com:/var/local/webserver
-scp -r images root@www.sugendran.com:/var/local/webserver
-ssh root@www.sugendran.com "chown -R root:www-data /var/local/webserver"
+aws s3 sync ./ s3://www.sugendran.com/
